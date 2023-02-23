ace2ebd792f28d80fd37b27ae2e7083213896d0b
Marwane Nemmassi <72465259+MarwaneNEMMASSI@users.noreply.github.com>
2023-02-23 16:57:34 +0100

    Add files via upload

--- profile.php
+++ profile.php
@@ -1,7 +1,17 @@
 <?php
-    session_start();
-    $id_session = session_id();
+
+session_start();
+
+if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+    // ... display user dashboard ...
+    echo $user_id;
+} else {
+    header('Location: Connection.php');
+    exit;
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,61 +26,65 @@
 </head>
 
 <body>
-
-    <?php echo $id_session;?>
-    <?php if(!isset($_SESSION["Logged_User_ID"])): ?>
     <header>
         <h1>
             My Home
         </h1>
-
     </header>
-
-    <?php else: ?>
-    <header>
-        <h1>
-            My Home
-        </h1>
-            <span> <?php echo $_SESSION["Logged_User_email"]?></span>         
-    </header>
-    <?php endif ?>
-
     <nav>
         <ul>
-            <li><a href="index.php"><i class="fa-solid fa-house"></a></i></li>
+            <li><a href="http://localhost/knn/folf1/inde.php"><i class="fa-solid fa-house"></a></i></li>
             <li>
-                <a href="profile.php"><i class="fa-solid fa-user"></i></i></a>
+                <a href="http://localhost/knn/folf1/profile.php"><i class="fa-solid fa-user"></i></i></a>
             </li>
             <li>
-                <a href="logout.php"><i class="fa-solid fa-right-from-bracket"></i></a>
+
+                <a href="http://localhost/knn/folf1/exit.php">
+                    <i class="fa-solid fa-right-from-bracket"></i>
+                </a>
             </li>
-            <li><a href="ajoute.php"><i class="fa-solid fa-plus"></i></a></li>
+
+            <li><a href="http://localhost/knn/folf1/ajoute.php"><i class="fa-solid fa-plus"></i></a></li>
 
         </ul>
     </nav>
 
 
     <?php
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "gestionnaire";
-            // Créez une connexion
-            $conn = mysqli_connect($servername, $username, $password, $dbname);
-            // Vérifiez la connexion
-            if (!$conn) {
-                die("Connexion échouée : " . mysqli_connect_error());
-            }
 
 
-            $sql = "SELECT * FROM clients WHERE `id_client`=1 ";
+    $dsn = 'mysql:host=localhost;dbname=gestionnaire';
+    $username = 'root';
+    $password = '';
+    $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    );
+    try {
+        $db = new PDO($dsn, $username, $password, $options);
+    } catch (PDOException $e) {
+        echo 'Connection failed: ' . $e->getMessage();
+    }
 
-            $result = mysqli_query($conn, $sql);
-            if (mysqli_num_rows($result) > 0) {
+    // Query the database for the first 5 records
+    $stmt = $db->prepare('SELECT * FROM clients WHERE id_client = :id LIMIT 5');
+    $stmt->bindParam(':id', $user_id, PDO::PARAM_INT);
+    $stmt->execute();
+    $records = $stmt->fetchAll();
 
-                while ($row = mysqli_fetch_assoc($result)) {
+    // Display the records on the page
+    foreach ($records as $record) {
 
-                    echo '
+
+
+
+
+
+
+
+
+
+
+        echo '
                      
                     <section class="m-0 mt-5">
                     <div class="d-flex  justify-content-center align-content-center ">
@@ -89,41 +103,41 @@
                                 <div class="row">
                                     <div class="col-sm-5 col-md-6">
                                         <label for="" class="form-label">Nome</label>
-                                        <input type="text" class="form-control input" value="'.$row['nom'].'" name="">
+                                        <input type="text" class="form-control input" value="' . $record['nom'] . '" name="nom">
             
                                     </div>
                                     <div class="col-sm-5 offset-sm-2 col-md-6 offset-md-0">
                                         <label for="" class="form-label">Prenom</label>
-                                        <input type="text" class="form-control input" value="'.$row['prenom'].'" name="">
+                                        <input type="text" class="form-control input" value="' . $record['prenom'] . '" name="prenom">
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-sm-6 col-md-5 col-lg-6">
                                         <label for="" class="form-label">Email
                                         </label>
-                                        <input type="text" class="form-control value="'.$row['email'].'" input" name="">
+                                        <input type="text" class="form-control input" value="' . $record['email'] . '" name="email">
             
                                     </div>
                                     <div class="col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0">
                                         <label for="" class="form-label">Téléphone</label>
-                                        <input type="text" value="'.$row['telephone'].'" class="form-control input" name="">
+                                        <input type="text" value="' . $record['telephone'] . '" class="form-control input" name="telephone">
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-sm-5 col-md-6">
                                         <label for="" class="form-label">Mot de passe</label>
-                                        <input type="password" value="'.$row['password'].'" class="form-control input" name="">
+                                        <input type="password" value="' . $record['password'] . '" class="form-control input" name="password">
             
                                     </div>
                                     <div class="col-sm-5 offset-sm-2 col-md-6 offset-md-0">
                                         <label for="" class="form-label">Confirmer le mot de passe</label>
-                                        <input type="password" value="'.$row['password'].'" class="form-control input" name="">
+                                        <input type="password" value="' . $record['password'] . '" class="form-control input" name="">
                                     </div>
                                 </div>
                             </div>
                             <div class="d-flex  justify-content-center align-content-center ">
                                 <div>
-                                    <input type="submit" class="form-control inpu my-5" name="" value="Update">
+                                    <input type="submit" class="form-control inpu my-5" name="Update" value="Update">
                                 </div>
             
             
@@ -132,18 +146,15 @@
                     </form>
 
             ';
-                }
-            } else {
-                echo "0 résultats";
-            }
+    }
 
 
 
 
 
 
-            ?>
-    
+    ?>
+
 
 
 
@@ -153,53 +164,100 @@
         <h1>Mes annonce</h1>
         <div id="cards">
             <?php
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "gestionnaire";
-            // Créez une connexion
-            $conn = mysqli_connect($servername, $username, $password, $dbname);
-            // Vérifiez la connexion
-            if (!$conn) {
-                die("Connexion échouée : " . mysqli_connect_error());
-            }
+            $stmt = $db->prepare('SELECT * FROM annonce WHERE id_client = :id ');
+            $stmt->bindParam(':id', $user_id, PDO::PARAM_INT);
+            $stmt->execute();
+            $records = $stmt->fetchAll();
 
+            // Display the records on the page
+            foreach ($records as $record) {
+                echo '
+            <div style="display: flex; flex-direction:column;">
 
-            $sql = "SELECT * FROM annonce WHERE `id_client`=1 ";
-
-            $result = mysqli_query($conn, $sql);
-            if (mysqli_num_rows($result) > 0) {
-
-                while ($row = mysqli_fetch_assoc($result)) {
-
-                    echo '
-                     
+            <div >
                 <form action=" http://localhost/knn/folf1/info.php" method="post" >
                 <div class="card" style="width: 15rem;" >
                 <img src="img5.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title">' . $row['titre'] . '</h5>
-                  <p class="card-text">' . $row['prix'] . '</p>
-                  <p class="card-text">' . $row['adresse'] . '</p>
-                  <input type="hidden" name="id"  value="' . $row["id_annonce"] . '">
-                  <input type="submit" class="btn btn-primary" name="modal"  value="more">
+                  <h5 class="card-title">' . $record['titre'] . '</h5>
+                  <p class="card-text">' . $record['prix'] . '</p>
+                  <p class="card-text">' . $record['adresse'] . '</p>
+                  <input type="hidden" name="id"  value="' . $record["id_annonce"] . '">
+                  <input type="submit" class="btn btn-outline-primary more" name="modal"  value="more">
 
                 </div>
+                    </form>
+                    
+                    <div style="display: flex; ">
+                    <!-- Button trigger modal -->
+                    <button type="button" class="p-3 mb-2 btn btn-outline-danger ms-3 me-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Supprimer
+                    </button>
+                    <form action=" http://localhost/knn/folf1/modif.php" method="post" >
+                        <input type="hidden" name="id"  value="' . $record["id_annonce"] . '">
+                        <input type="submit" class="p-3 mb-2 btn btn-outline-success" name="modifier"  value="Modifier">
+                    </form>
+                    </div>
+                
               </div>
-              </form>
+              
+              
+            </div>
+            </div>
+                    <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Suppression</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>Etes-vous sur de vouloir supprimer cette annonce ?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="p-3 mb-2 btn btn-outline-secondary" data-bs-dismiss="modal">Non</button>
+                <form action=" http://localhost/knn/folf1/suprimer.php" method="post">
+                    <input type="hidden" name="id"   value="' . $record["id_annonce"] . '">
+                    <input type="submit" class="p-3 mb-2 btn btn-outline-danger ms-3 me-2" name="suprimer"  value="Oui">
+                </form>
+            </div>
+            </div>
+        </div>
+        </div>
+                     
               
 
             ';
-                }
-            } else {
-                echo "0 résultats";
             }
+            // Update the client's information
+            if (isset($_POST['Update'])) {
+                $value1 = $_POST['nom'];
+                $value2 = $_POST['prenom'];
+                $value3 = $_POST['email'];
+                $value4 = $_POST['password'];
+                $value5 = $_POST['telephone'];
+                $value6 = $user_id;
 
+                try {
+                    $stmt = $db->prepare("UPDATE clients SET nom = :value1, prenom = :value2, email = :value3, password = :value4, telephone = :value5 WHERE id_client = :value6");
 
+                    $stmt->bindParam(':value1', $value1);
+                    $stmt->bindParam(':value2', $value2);
+                    $stmt->bindParam(':value3', $value3);
+                    $stmt->bindParam(':value4', $value4);
+                    $stmt->bindParam(':value5', $value5);
+                    $stmt->bindParam(':value6', $value6);
 
-
-
-
+                    $stmt->execute();
+                    header('Location: profile.php');
+                    exit;
+                } catch (PDOException $e) {
+                    echo "Error updating data: " . $e->getMessage();
+                }
+            }
+            // Close connection
+            $conn = null;
             ?>
         </div>
     </section>
